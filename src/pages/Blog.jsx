4cf587df335cb401fa29b2952e942fa478e516a8
Arunarchitect <intformbase@gmail.com>
2023-11-22 11:14:44 +0530

    added edit delete ui in blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,32 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
-import { Grid } from '@mui/material';
+import { Grid, Popover, MenuItem, IconButton } from '@mui/material';
 import Card from '@mui/material/Card';
-import { useParams } from 'react-router-dom';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import '../styles/Dash.css';
 
 import { useGetResumeprofileQuery } from '../services/testApi';
 
 const Blog = () => {
-
-
-  // Fetch data from Django API
   const { data, isSuccess } = useGetResumeprofileQuery();
-
   const [blogs, setBlogs] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedBlog, setSelectedBlog] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEditClick = () => {
+    // Handle edit logic for the selected blog (e.g., navigate to the edit page)
+    console.log('Edit clicked for blog:', selectedBlog);
+    handleClose();
+  };
+
+  const handleDeleteClick = () => {
+    // Handle delete logic for the selected blog (e.g., show a confirmation dialog)
+    console.log('Delete clicked for blog:', selectedBlog);
+    handleClose();
+  };
 
   useEffect(() => {
     if (isSuccess) {
-      // Assuming your API response has a 'blogs' field containing the required data
       setBlogs(data.blogs);
     }
   }, [data, isSuccess]);
@@ -36,7 +53,7 @@ const Blog = () => {
       <Grid container spacing={2} padding={3} style={{ backgroundColor: 'gray' }} justifyContent="center">
         {blogs.map((blog, index) => (
           <Grid key={index} item xs={12} lg={4} sm={4} padding={2}>
-            <Link to={`/post/${blog.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+            
               <Card
                 sx={{
                   width: '100%',
@@ -51,24 +68,57 @@ const Blog = () => {
               >
                 <CardHeader
                   avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label={blog.avatarLabel}>{blog.avatarLabel}</Avatar>}
-                  action={<IconButton aria-label="settings"><MoreVertIcon /></IconButton>}
-                  title={blog.title}
-                  subheader={blog.subheader}
+                  action={
+                    <div>
+                      <IconButton aria-label="settings" onClick={(event) => { handleClick(event); setSelectedBlog(blog); }}>
+                        <MoreVertIcon />
+                      </IconButton>
+                      <Popover
+                        open={Boolean(anchorEl)}
+                        anchorEl={anchorEl}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                          vertical: 'bottom',
+                          horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'right',
+                        }}
+                      >
+                        <MenuItem onClick={handleEditClick}>
+                          <EditIcon />
+                          Edit
+                        </MenuItem>
+                        <MenuItem onClick={handleDeleteClick}>
+                          <DeleteIcon />
+                          Delete
+                        </MenuItem>
+                      </Popover>
+                    </div>
+                  }
+                  title={
+                    <Link to={`/post/${blog.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <Typography variant="h7">{blog.title}</Typography>
+                      <Typography variant="subtitle1" style={{ fontSize: '0.8em' }}>{blog.subtitle}</Typography>
+                      
+                    </Link>
+                  }
                 />
-                {/* Use CardMedia to display the image */}
-                <CardMedia
-                  component="img"
-                  alt={blog.title}
-                  height="140" // Set the desired height
-                  image={`https://api.arunarchitect.in/${blog.bimage}`}
-                />
+                <Link to={`/post/${blog.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  <CardMedia
+                    component="img"
+                    alt={blog.title}
+                    height="140" // Set the desired height
+                    image={`https://api.arunarchitect.in/${blog.bimage}`}
+                  />
+                </Link>
                 <CardContent>
                   <Typography variant="body2" color="text.secondary">
                     {blog.subheader}
                   </Typography>
                 </CardContent>
               </Card>
-            </Link>
           </Grid>
         ))}
       </Grid>
